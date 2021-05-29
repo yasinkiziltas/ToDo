38061bd9ef785790aa0a55a79bebdc4e5fc65777
yasinkiziltas <yyasinkiziltas@hotmail.com>
2021-05-29 15:53:07 +0300

    Todo Types dropdown

--- components/Main.js
+++ components/Main.js
@@ -18,7 +18,7 @@ const Drawer = createDrawerNavigator()
 export default function Main(navigation) {
 
     return (
-        <Drawer.Navigator initialRouteName="Home" drawerContent={props => <DrawerContent {...props} />} >
+        <Drawer.Navigator initialRouteName="Add" drawerContent={props => <DrawerContent {...props} />} >
 
             <Drawer.Screen name="Home" component={HomeScreen}
                 options={{
--- components/main/AddTodoScreen.js
+++ components/main/AddTodoScreen.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, SafeAreaView } from 'react-native'
 import firebase from 'firebase'
 import CustomHeader from '../CustomHeader'
 import { useTheme } from '@react-navigation/native'
 import DatePicker from 'react-native-datepicker';
+import SearchableDropdown from 'react-native-searchable-dropdown';
 
 export default function AddTodoScreen({ navigation }) {
 
     const [todo, setTodo] = useState(null);
     const [title, setTitle] = useState(null);
     const [date, setDate] = useState('21-10-2021')
+    const [data, setData] = useState([])
+    const [todoType, setTodoType] = useState(null)
     const { colors } = useTheme()
+
+    const items = [
+        { id: 1, name: 'Personal' },
+        { id: 2, name: 'Business' }
+    ];
 
     // const submitTodo = () => {
     //     firebase.firestore()
@@ -39,7 +47,8 @@ export default function AddTodoScreen({ navigation }) {
                 // title: title,
                 todo: todo,
                 date: date,
-                todoTime: firebase.firestore.Timestamp.fromDate(new Date())
+                todoTime: firebase.firestore.Timestamp.fromDate(new Date()),
+                todoType: todoType
             }).then(() => {
                 console.log('Todo Added!')
                 Alert.alert(
@@ -48,6 +57,11 @@ export default function AddTodoScreen({ navigation }) {
                 )
             })
     }
+
+    useEffect(() => {
+        setData(items)
+        console.log(items)
+    }, [])
 
 
     return (
@@ -87,6 +101,51 @@ export default function AddTodoScreen({ navigation }) {
                     confirmBtnText="Confirm"
                     cancelBtnText="Cancel"
                 />
+
+                <SearchableDropdown
+                    // onTextChange={(text) => setTodoType(text)}
+                    //On text change listner on the searchable input
+                    onItemSelect={(item) => setTodoType(item)}
+                    //onItemSelect called after the selection from the dropdown
+                    containerStyle={{ padding: 5 }}
+                    //suggestion container style
+                    textInputStyle={{
+                        //inserted text style
+                        width: 150,
+                        padding: 12,
+                        borderWidth: 1,
+                        borderColor: '#ccc',
+                        backgroundColor: '#FAF7F6',
+                    }}
+                    itemStyle={{
+                        //single dropdown item style
+                        padding: 10,
+                        marginTop: 2,
+                        backgroundColor: '#FAF9F8',
+                        borderColor: '#bbb',
+                        borderWidth: 1,
+                    }}
+                    itemTextStyle={{
+                        //text style of a single dropdown item
+                        color: '#222',
+                    }}
+                    itemsContainerStyle={{
+                        //items container style you can pass maxHeight
+                        //to restrict the items dropdown hieght
+                        maxHeight: '60%',
+                    }}
+                    items={items}
+                    //mapping of item array
+                    defaultIndex={2}
+                    //default selected item index
+                    placeholder="Select type todo"
+                    //place holder for the search input
+                    resetValue={false}
+                    //reset textInput Value with true and false state
+                    underlineColorAndroid="transparent"
+                //To remove the underline from the android input
+                />
+
 
                 <TouchableOpacity style={styles.addPostButton} onPress={submitTodo}>
                     <Text style={[styles.addPostButtonText]}>New task  ^</Text>
